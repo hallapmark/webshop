62a1a6c14fd28971e168b09cd1b2efb08f8dd110
Mark Hallap <hallapmark@gmail.com>
2025-11-23 23:03:10 +0200

    Add a Users page

--- src/pages/lists/Users.jsx
+++ src/pages/lists/Users.jsx
@@ -1,6 +1,31 @@
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography'
+import { useState } from 'react';
+import usersFile from '../../data/users.json'
+
+
 function Users() {
-  return (
-    <div>Users</div>
-  )
+    const [users, setUsers] = useState(usersFile.slice());
+      
+    return (
+      <Box sx={{ textAlign: "center" }}>
+        <Typography variant="h1">Users</Typography>
+        <Box display="flex" justifyContent="center">
+          <List>
+            {users.map((user) => (
+              <ListItem key={user.id}>
+                <ListItemText
+                  primary={user.name}
+                  secondary={user.email}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Box>
+    )
 }
 export default Users
