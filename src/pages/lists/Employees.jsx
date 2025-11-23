4d9b36e9efaf1bc0bc4d6d0dc34c81e86f0514c9
Mark Hallap <hallapmark@gmail.com>
2025-11-23 22:28:12 +0200

    Add main title to Employees page

--- src/pages/lists/Employees.jsx
+++ src/pages/lists/Employees.jsx
@@ -11,36 +11,39 @@ function Employees() {
   const [employees, setEmployees] = useState(employeesFile.slice());
   
   return (
-    <Grid container spacing={4} alignItems="stretch" mx={2} my={4}>
-      {employees.map((employee) => (
-        <Grid 
-          size={{xs: 12, sm: 6, md: 4, lg: 3}} 
-          sx={{ display: "flex", flexDirection: "column"}} 
-          key={employee.id}
-        >
-          <Card sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
-            <CardMedia
-              component="img"
-              image={`src/assets/${employee.image}`}
-              alt="Employee"
-              sx={{
-                maxHeight: 140,
-                objectFit: "contain", // maintain aspect ratio
-                mx: "auto",           // center horizontally
-                mt: 2
-              }}
-            />
-            <CardContent sx={{ textAlign: "center", flexGrow: 1}}>
-              <Typography variant="h5" >{employee.name}</Typography>
-            </CardContent>
-          </Card>
-          <Box sx={{ textAlign: "center", mt: 1.5 }}>
-            <Typography variant="body2">{employee.email}</Typography>
-            <Typography variant="body2">{employee.phone}</Typography>
-          </Box>
-        </Grid>
-      ))}
-    </Grid>
+    <Box>
+      <Typography variant="h1">Employees</Typography>
+      <Grid container spacing={4} alignItems="stretch" mx={2} my={4}>
+        {employees.map((employee) => (
+          <Grid
+            size={{xs: 12, sm: 6, md: 4, lg: 3}}
+            sx={{ display: "flex", flexDirection: "column"}}
+            key={employee.id}
+          >
+            <Card sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+              <CardMedia
+                component="img"
+                image={`src/assets/${employee.image}`}
+                alt="Employee"
+                sx={{
+                  maxHeight: 140,
+                  objectFit: "contain", // maintain aspect ratio
+                  mx: "auto",           // center horizontally
+                  mt: 2
+                }}
+              />
+              <CardContent sx={{ textAlign: "center", flexGrow: 1}}>
+                <Typography variant="h5" >{employee.name}</Typography>
+              </CardContent>
+            </Card>
+            <Box sx={{ textAlign: "center", mt: 1.5 }}>
+              <Typography variant="body2">{employee.email}</Typography>
+              <Typography variant="body2">{employee.phone}</Typography>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   )
 }
 export default Employees
