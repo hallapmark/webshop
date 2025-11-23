5d38a50b988215517f6c0ea03ffb27de516bcf69
Mark Hallap <hallapmark@gmail.com>
2025-11-23 14:23:59 +0200

    Add the recommended mui css reset

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,13 @@ import TopNav from "./components/TopNav";
 import { Route, Routes } from "react-router-dom";
 import CarsPLP from "./pages/lists/CarsPLP";
 import Home from "./pages/Home";
+import { CssBaseline } from "@mui/material";
 
 function App() {
   return (
     <ThemeProvider theme={travelTheme}>
+      {/* see https://mui.com/material-ui/react-css-baseline/ */}
+      <CssBaseline />
       <TopNav />
       
       <Routes>
